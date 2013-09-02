1c297ee08a57bef64c73c3f98b07f841c733011f
Olivier Louvignes <olivier@mg-crea.com>
2013-09-02 19:47:04 +0200

    docs(global): working draft with buttons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,12 +39,79 @@ angular.module('docs', ['ngRoute', 'ngAnimate', 'ngWidgets.bootstrap'])
 
   // })
 
+
+  .directive('wgCodeExample', function($window) {
+
+    var jqLite = angular.element;
+    var concat = Array.prototype.concat;
+    var slice = Array.prototype.slice;
+
+    var entityMap = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      '\'': '&#39;',
+      '/': '&#x2F;'
+    };
+
+    function escapeHtml(string) {
+      return String(string).replace(/[&<>"'\/]/g, function (s) {
+        return entityMap[s];
+      });
+    }
+
+    return {
+      restrict: 'EAC',
+      compile: function postLink(element, attr) {
+
+        var children = slice.call(element[0].childNodes)
+        .filter(function(child) {
+          return child.outerHTML && child.nodeName.toLowerCase() !== 'pre';
+        }).map(function(child) {
+          return escapeHtml(child.outerHTML);
+        });
+
+        var pre = children.join('');
+
+        // Remove closing tag indentation to every line
+        if(pre.indexOf('\n') !== -1) {
+          var ws = pre.split('\n').pop().indexOf('&lt;');
+          var s = ''; while(ws--) s+=' ';
+          var p = new RegExp('\n' + s, ['g']);
+          pre = pre.replace(p, '\n');
+        }
+
+        pre = pre.replace(/=&quot;&quot;/g, '');
+        pre = pre.replace(/\n/g, '<br>');
+        pre = pre.replace(/  /g, '&nbsp;&nbsp;');
+
+        var highlighted = jqLite('<div class="highlight"></div>').html('<pre>' + pre + '</pre>');
+        $window.hljs && $window.hljs.highlightBlock(highlighted[0], null, true);
+        element.after(highlighted);
+
+      }
+    };
+
+  })
+
   .run(function($rootScope, $location, $anchorScroll, $routeParams, $route, debounce) {
 
     $rootScope.getRouteParam = function(param) {
       if(!$route.current || ! $route.current.$$route) return;
       return $route.current.$$route[param];
     };
+
+    var $scope = $rootScope;//.$new();
+
+    // Tooltip
+    $scope.tooltip = {title: 'Hello Tooltip<br />This is a multiline message!', checked: false};
+
+    // Buttons
+    $scope.button = {active: true};
+    $scope.buttonSelect = {currency: '¥'};
+    $scope.checkbox = {left: false, middle: true, right: false};
+    $scope.radio = {value: 'middle'};
 
     // var debouncedScroll = debounce(function() {
     //   var elm = document.getElementById($routeParams.page);
