70740897f8b35c403462219b46152cb42c4c3761
Olivier Louvignes <olivier@mg-crea.com>
2013-09-05 16:53:27 +0200

    feat(buttons): provide wgRadio

---
+++ src/buttons/buttons.js
@@ -0,0 +1,53 @@
+'use strict';
+
+angular.module('ngWidgets.bootstrap.buttons', [])
+
+  .constant('buttonsConfig', {
+    activeClass:'active',
+    toggleEvent:'click'
+  })
+
+  .directive('wgRadio', function buttonCheckbox(buttonsConfig) {
+
+    var isDefined = angular.isDefined;
+    var activeClass = buttonsConfig.activeClass || 'active';
+    var toggleEvent = buttonsConfig.toggleEvent || 'click';
+    var constantValueRegExp = /^(true|false|\d+)$/;
+
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function postLink(scope, element, attr, controller) {
+
+        // Support label > input[type="radio"]
+        var isInput = element[0].nodeName === 'INPUT';
+        var activeElement = isInput ? element.parent() : element;
+
+        var value = constantValueRegExp.test(attr.value) ? scope.$eval(attr.value) : attr.value;
+        console.warn('value', typeof value, value);
+
+        // model -> view
+        controller.$render = function () {
+          console.warn('$render', element.attr('value'), 'controller.$modelValue', typeof controller.$modelValue, controller.$modelValue, 'controller.$viewValue', typeof controller.$viewValue, controller.$viewValue);
+          var isActive = angular.equals(controller.$modelValue, value);
+          console.warn('isActive', isActive);
+          if(isInput) {
+            element[0].checked = isActive;
+          }
+          activeElement.toggleClass(activeClass, isActive);
+        };
+
+        // view -> model
+        element.bind(toggleEvent, function() {
+          scope.$apply(function () {
+            console.warn('!click', element.attr('value'), 'controller.$viewValue', typeof controller.$viewValue, controller.$viewValue, 'controller.$modelValue', typeof controller.$modelValue, controller.$modelValue);
+            controller.$setViewValue(value);
+            controller.$render();
+          });
+        });
+
+      }
+
+    };
+
+  });
