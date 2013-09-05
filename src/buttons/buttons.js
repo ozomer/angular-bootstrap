b8c1c48b3e9c688d144eaa82e299ecec6cf87c7e
Olivier Louvignes <olivier@mg-crea.com>
2013-09-05 16:07:23 +0200

    feat(buttons): provide wgCheckbox

---
+++ src/buttons/buttons.js
@@ -0,0 +1,75 @@
+'use strict';
+
+angular.module('ngWidgets.bootstrap.buttons', [])
+
+  .constant('buttonsConfig', {
+    activeClass:'active',
+    toggleEvent:'click'
+  })
+
+  .directive('wgCheckbox', function(buttonsConfig) {
+
+    var isDefined = angular.isDefined;
+    var activeClass = buttonsConfig.activeClass || 'active';
+    var toggleEvent = buttonsConfig.toggleEvent || 'click';
+    var constantValueRegExp = /^(true|false|\d+)$/;
+
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function postLink(scope, element, attr, controller) {
+
+        // Support label > input[type="checkbox"]
+        var isInput = element[0].nodeName === 'INPUT';
+        var activeElement = isInput ? element.parent() : element;
+
+        var trueValue = isDefined(attr.trueValue) ? attr.trueValue : true;
+        if(constantValueRegExp.test(attr.trueValue)) {
+          trueValue = scope.$eval(attr.trueValue);
+        }
+        var falseValue = isDefined(attr.falseValue) ? attr.falseValue : false;
+        if(constantValueRegExp.test(attr.falseValue)) {
+          falseValue = scope.$eval(attr.falseValue);
+        }
+
+        // Parse exotic values
+        var hasExoticValues = typeof trueValue !== 'boolean' || typeof falseValue !== 'boolean';
+        if(hasExoticValues) {
+          controller.$parsers.push(function(viewValue) {
+            console.warn('$parser', element.attr('ng-model'), 'viewValue', viewValue);
+            return viewValue ? trueValue : falseValue;
+          });
+          // Fix rendering for exotic values
+          scope.$watch(attr.ngModel, function(newValue, oldValue) {
+            controller.$render();
+          });
+        }
+
+        // model -> view
+        controller.$render = function () {
+          console.warn('$render', element.attr('ng-model'), 'controller.$modelValue', typeof controller.$modelValue, controller.$modelValue, 'controller.$viewValue', typeof controller.$viewValue, controller.$viewValue);
+          var isActive = angular.equals(controller.$modelValue, trueValue);
+          if(isInput) {
+            element[0].checked = isActive;
+          }
+          activeElement.toggleClass(activeClass, isActive);
+        };
+
+        // view -> model
+        element.bind(toggleEvent, function() {
+          scope.$apply(function () {
+            console.warn('!click', element.attr('ng-model'), 'controller.$viewValue', typeof controller.$viewValue, controller.$viewValue, 'controller.$modelValue', typeof controller.$modelValue, controller.$modelValue);
+            if(!isInput) {
+              controller.$setViewValue(!activeElement.hasClass('active'));
+            }
+            if(!hasExoticValues) {
+              controller.$render();
+            }
+          });
+        });
+
+      }
+
+    };
+
+  });
