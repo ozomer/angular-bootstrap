ab0a3e61efb80dce462457591b665c20fa35c521
Oren Zomer <oren.zomer@gmail.com>
2013-11-03 16:12:41 +0200

    fix(affix): initialize only after first content is loaded and window is ready.

--- src/affix/affix.js
+++ src/affix/affix.js
@@ -71,25 +71,37 @@ angular.module('ngWidgets.bootstrap.affix', ['ngWidgets.bootstrap.jqlite.dimensi
         }
 
         $affix.init = function() {
-          var firstLoad = true;
-          $rootScope.$on("$viewContentLoaded", function() {
-            // All initializations that are based on offsets and sizes should
-            // be called only after the DOM is built.
-            if (firstLoad) {
+          var isInited = false;
+          var isWindowReady = false;
+          var isContentLoaded = false;
+
+          function refreshAffix() {
+            // Only when the window is ready and the content is loaded,
+            // we run the initialization process.
+            if (!(isWindowReady && isContentLoaded)) {
+              return;
+            }
+            if (!isInited) {
               // Updating intialOffsetTop every time content is loaded is problematic, 
               // because it depends on the element's current top-offset and not on the
               // original value (i.e. when its location is not fixed).
               // Hopefully, the content is not loaded above the element, or the 
-              // "offsetParent" mode is set.
+              // "offsetParent" mode is set (and then initialOffsetTop is useless).
               initialOffsetTop = dimensions.offset(element[0]).top + initialAffixTop;
-              // Bind events
               windowEl.on('scroll', $affix.checkPosition);
-              windowEl.on('click', $affix.checkPosition);
-              firstLoad = false;
-            } 
+              windowEl.on('click', $affix.checkPosition);              
+              isInited = true;
+            }
             $affix.updateOffsets();
-            
             $affix.checkPosition();
+          }
+          $rootScope.$on("$viewContentLoaded", function() {
+            isContentLoaded = true;
+            refreshAffix();
+          });
+          windowEl.ready(function() {
+            isWindowReady = true;
+            refreshAffix();
           });
         };
         
